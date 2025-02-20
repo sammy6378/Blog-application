705442f9f1b4194e5aab9dda0b12f632ec41fcab
Bil <bildadsimiyu6@gmail.com>
2025-02-20 16:32:30 +0300

    format

--- client/components/Navbar.tsx
+++ client/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { ChevronDown, Sun, Moon, Menu, X } from "lucide-react";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-import OutsideClickHandler from 'react-outside-click-handler'
+import OutsideClickHandler from "react-outside-click-handler";
 
 export default function Navbar() {
   const { theme, setTheme } = useTheme();
@@ -27,44 +27,48 @@ export default function Navbar() {
           />
         </Link>
         <div className="flex justify-between gap-[50px] max-800:gap-[30px] max-700:gap-[25px] ">
-            <OutsideClickHandler onOutsideClick={() => setBar(false)}>
-          <ul className={`flex items-center gap-5 font-[500] max-800:gap-3 max-700:block max-700:fixed max-700:top-[20px] max-700:dark:bg-white max-700:bg-gray-900 max-700:text-white max-700:dark:text-black max-700:p-2 max-700:w-[180px] max-700:max-w-full max-700:rounded max-700:right-2 max-300px:right-0 max-700:z-50 max-700:pt-4 max-700:h-[200px] max-200px:text-sm ${bar ? "max-700:block" : "max-700:hidden"}`}>
-            <li className="max-700:mt-2 max-700:mb-2">
-              <Link
-                href={"/"}
-                className={`hover:text-crimson dark:hover:text-green transition max-700:text-white max-700:dark:text-black`}
-              >
-                Home
-              </Link>
-            </li>
-            <li className="flex items-center hover:text-crimson dark:hover:text-green transition cursor-pointer text-black dark:text-white max-700:dark:text-black max-700:text-white max-700:my-5">
-              <span>Category</span>
-              <ChevronDown className="" size={15} />
-            </li>
-            <li className="max-700:mb-5">
-              <Link
-                href={"/contact"}
-                className={`hover:text-crimson dark:hover:text-green transition`}
-              >
-                Contact Us
-              </Link>
-            </li>
-            <button className="dark:bg-green hover:opacity-90 bg-crimson px-2 py-1 rounded duration-500 700:hidden ">
-              Login
-            </button>
-          </ul>
-</OutsideClickHandler>
+          <OutsideClickHandler onOutsideClick={() => setBar(false)}>
+            <ul
+              className={`flex items-center gap-5 font-[500] max-800:gap-3 max-700:block max-700:fixed max-700:top-[20px] max-700:dark:bg-white max-700:bg-gray-900 max-700:text-white max-700:dark:text-black max-700:p-2 max-700:w-[180px] max-700:max-w-full max-700:rounded max-700:right-2 max-300px:right-0 max-700:z-50 max-700:pt-4 max-700:h-[200px] max-200px:text-sm ${bar ? "max-700:block" : "max-700:hidden"}`}
+            >
+              <li className="max-700:mt-2 max-700:mb-2">
+                <Link
+                  href={"/"}
+                  className={`hover:text-crimson dark:hover:text-green transition max-700:text-white max-700:dark:text-black`}
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="flex items-center hover:text-crimson dark:hover:text-green transition cursor-pointer text-black dark:text-white max-700:dark:text-black max-700:text-white max-700:my-5">
+                <span>Category</span>
+                <ChevronDown className="" size={15} />
+              </li>
+              <li className="max-700:mb-5">
+                <Link
+                  href={"/contact"}
+                  className={`hover:text-crimson dark:hover:text-green transition`}
+                >
+                  Contact Us
+                </Link>
+              </li>
+              <button className="dark:bg-green hover:opacity-90 bg-crimson px-2 py-1 rounded duration-500 700:hidden ">
+                Login
+              </button>
+            </ul>
+          </OutsideClickHandler>
           <div className="flex gap-4 items-center">
             {/* switch modes */}
             {theme === "light" ? (
               <Moon
                 onClick={() => setTheme("dark")}
-                className="cursor-pointer text-black dark:text-white hover:text-slate-800" fill="black"
+                className="cursor-pointer text-black dark:text-white hover:text-slate-800"
+                fill="black"
               />
             ) : (
               <Sun
                 onClick={() => setTheme("light")}
-                className="cursor-pointer hover:text-slate-300 dark:text-white text-black" fill="black"
+                className="cursor-pointer hover:text-slate-300 dark:text-white text-black"
+                fill="black"
               />
             )}
 
@@ -75,7 +79,10 @@ export default function Navbar() {
                 onClick={() => setBar(!bar)}
               />
             ) : (
-              <span className="cursor-pointer z-[100] dark:text-white" onClick={() => setBar(!bar)}>
+              <span
+                className="cursor-pointer z-[100] dark:text-white"
+                onClick={() => setBar(!bar)}
+              >
                 <HiOutlineMenuAlt3 size={24} />
               </span>
             )}
