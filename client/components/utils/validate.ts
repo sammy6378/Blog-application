325adfa656f4922b650d077bd3a0f4201efb2976
sammy6378 <samuel202mwangi@gmail.com>
2025-02-24 10:43:38 +0300

    feat: add validation schemas for registration and login forms using Yup

---
+++ client/components/utils/validate.ts
@@ -0,0 +1,19 @@
+import * as yup from 'yup';
+
+// email regex
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+// password regex
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+export const validateRegisterSchema = yup.object().shape({
+    name: yup.string().required('Name is required').trim(),
+    email: yup.string().matches(emailRegex, 'Invalid Email').required('Email is required'),
+    password: yup.string().matches(passwordRegex, 'Password must include at least one number, one uppercase letter, one lowercase letter, and one special character').required('Password is required')
+})
+
+
+export const ValidateLoginSchema = yup.object().shape({
+    email: yup.string().matches(emailRegex, 'Invalid Email').required('Email is required'),
+    password: yup.string().required('Password is required')
+})
