ba4c11c763b973bab098593f2ae927b45e2fc08a
Bil <bildadsimiyu6@gmail.com>
2025-03-11 17:42:59 +0300

    fix: update styling for Profile and Navbar components for improved visibility and layout

--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -24,7 +24,7 @@ export default function Profile() {
   }
   return (
     <section className="w-full max-h-screen flex items-center justify-center ">
-      <div className="bg-gray-800 -z-[10] text-white dark:text-gray-900 dark:bg-white h-1/2 w-[600px] max-w-[90%] flex flex-col items-center justify-center p-3 mt-4 rounded-md shadow">
+      <div className="bg-gray-800  text-white dark:text-gray-900 dark:bg-white h-1/2 w-[600px] max-w-[90%] flex flex-col items-center justify-center p-3 mt-4 rounded-md shadow">
         <section className="relative">
           <Image
             src={userInfo?.avatar?.url || data?.user?.image || "/profile.webp"}
@@ -47,7 +47,7 @@ export default function Profile() {
 
         <button className="mt-8 flex flex-col gap-2 dark:shadow border border-slate-500 dark:border-slate-300 rounded-md p-2 py-4 w-[90%] max-500:w-full items-center relative mb-4 bg-[#37a39a] hover:opacity-90 transition">Update Password</button>
 
-        <button className="flex flex-row justify-center gap-2 dark:shadow border border-slate-500 dark:border-slate-300 rounded-md p-2 py-4 w-[90%] max-500:w-full items-center relative mb-4 bg-crimson hover:opacity-90 transition"><span className="place-self-center">Delete Account</span><VscWarning className="absolute left-2" size={25} /></button>
+        <button className="flex flex-row justify-center gap-2 dark:shadow border-2 border-crimson text-crimson rounded-md p-2 py-4 w-[90%] max-500:w-full items-center relative mb-4 hover:bg-crimson hover:opacity-90 transition hover:text-white"><span className="place-self-center">Delete Account</span><VscWarning className="absolute left-2" size={25} /></button>
       </div>
     </section>
   );
