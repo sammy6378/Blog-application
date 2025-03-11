c073268569b70605a5873c80b1c47444ceeb0b7a
sammy6378 <samuel202mwangi@gmail.com>
2025-03-11 10:09:57 +0300

    refactor: adjust profile page layout for improved styling and z-index

--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -11,8 +11,8 @@ export default function Profile() {
   const { userInfo } = useContextFunc();
   const { data } = useSession();
   return (
-    <section className="w-full max-h-screen flex items-center justify-center -z-10">
-      <div className="bg-gray-800 text-white dark:text-gray-900 dark:bg-white h-1/2 w-[600px] max-w-[90%] flex flex-col items-center justify-center p-3 mt-4 rounded-md shadow">
+    <section className="w-full max-h-screen flex items-center justify-center ">
+      <div className="bg-gray-800 -z-[10] text-white dark:text-gray-900 dark:bg-white h-1/2 w-[600px] max-w-[90%] flex flex-col items-center justify-center p-3 mt-4 rounded-md shadow">
         <section className="relative">
           <Image
             src={userInfo?.avatar?.url || data?.user?.image || "/profile.webp"}
@@ -26,7 +26,7 @@ export default function Profile() {
           <FaCamera className="absolute bottom-0 right-0 cursor-pointer" />
         </section>
         {/* name and email */}
-        <section className="mt-8 flex flex-col gap-2 dark:shadow border border-slate-500 dark:border-slate-300 rounded-md p-2 py-4 w-[90%] max-500:w-full items-center relative mb-4 bg-transparent">
+        <section className="mt-8 flex flex-col gap-2 dark:shadow border border-slate-500 dark:border-slate-300 rounded-md p-2 py-4 w-[90%] max-500:w-full items-center relative mb-4">
           <p className="font-semibold">{userInfo?.name}</p>
           <hr className="dark:w-[80%] dark:max-500:w-full gray-slate-500 w-0" />
           <p className="dark:text-gray-700 text-gray-400">{userInfo?.email}</p>
