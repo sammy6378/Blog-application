66db6f34aefd3346de3b7a404ef4183efa5200aa
Bil <bildadsimiyu6@gmail.com>
2025-03-23 14:24:32 +0300

    refactor: update dashboard with real data and add create blogs page

--- client/app/(ADMIN)/admin/dashboard/page.tsx
+++ client/app/(ADMIN)/admin/dashboard/page.tsx
@@ -2,12 +2,14 @@
 import { useContextFunc } from "@/components/context/AppContext";
 import AdminProtected from "@/components/utils/protected/AdminProtected";
 import Protected from "@/components/utils/protected/Protected";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 function Page() {
-  const { userInfo, userTotal, blogCount } = useContextFunc();
+  const { userInfo, userTotal, blogCount, blogs } = useContextFunc();
   const [greeting, setGreeting] = useState("");
   const [totalLikes, setTotalLikes] = useState(0);
+  const [totalDislikes, setTotalDislikes] = useState(0);
   const [totalComments, setTotalComments] = useState(0);
   const [averageRating, setAverageRating] = useState(0);
   const [topBlogs, setTopBlogs] = useState<any>([]);
@@ -25,6 +27,33 @@ function Page() {
     }
   }, [hour]);
 
+  useEffect(() => {
+    fetchBlogItems();
+    console.log(blogs?.length)
+  }, [blogs])
+
+  const fetchBlogItems = () => {
+    if(!blogs || blogs.length === 0) return;
+
+    //sum up all likes
+    const totalLikesCount = blogs?.reduce((acc: number, blog: any) => acc + blog.likes, 0);
+    setTotalLikes(totalLikesCount as number);
+
+    //dislikes
+    const totalDislikesCount = blogs?.reduce((acc: number, blog: any) => acc + blog.dislikes, 0);
+    setTotalDislikes(totalDislikesCount as number);
+
+    //rating
+    const totalRating = blogs?.reduce((acc: number, blog: any) => acc + blog.rating, 0);
+    const averageRating = blogs.length > 0 ? totalRating / blogs.length : 0;
+    setAverageRating(averageRating);
+    
+
+    //sum up all comments
+    const totalComments = blogs.reduce((acc: number, blog: any) => acc + blog.comments.length, 0);
+    setTotalComments(totalComments);
+  }
+
   return (
     <Protected>
       <AdminProtected>
@@ -39,7 +68,7 @@ function Page() {
           {/* display total users and blogs */}
           <section className="flex items-center justify-between 700:justify-center 800:gap-[25%] gap-[20px] my-[50px] max-500:flex-col">
             {/* users */}
-            <div className="flex flex-col items-center justify-center w-[180px] h-[150px] 800:w-[250px] 800:h-[150px] dark:bg-gray-900 bg-gray-100 rounded-md text-3xl 700:text-3xl 500:text-2xl font-semibold gap-[15px] dark:text-white shadow shadow-gray-400 dark:shadow-gray-700 hover:shadow-sm hover:-translate-y-1 transition-all ">
+            <div className="flex flex-col items-center justify-center w-[180px] h-[150px] 800:w-[250px] 800:h-[150px] dark:bg-gray-900 bg-gray-100 rounded-md text-3xl 700:text-3xl 500:text-2xl font-semibold gap-[15px] dark:text-white shadow shadow-gray-400 dark:shadow-gray-700 hover:shadow-sm hover:-translate-y-1 transition-all max-500:w-full">
               <span>Users</span>
               <span className="dark:text-green text-crimson">
                 {userTotal || 0}
@@ -47,7 +76,7 @@ function Page() {
             </div>
 
             {/* blogs */}
-            <div className="flex flex-col items-center justify-center w-[180px] h-[150px] 800:w-[250px] 800:h-[150px] dark:bg-gray-900 bg-gray-100 rounded-md text-3xl 700:text-3xl 500:text-2xl font-semibold gap-[15px] dark:text-white shadow shadow-gray-400 dark:shadow-gray-700 hover:shadow-sm hover:-translate-y-1 transition-all ">
+            <div className="flex flex-col items-center justify-center w-[180px] h-[150px] 800:w-[250px] 800:h-[150px] dark:bg-gray-900 bg-gray-100 rounded-md text-3xl 700:text-3xl 500:text-2xl font-semibold gap-[15px] dark:text-white shadow shadow-gray-400 dark:shadow-gray-700 hover:shadow-sm hover:-translate-y-1 transition-all max-500:w-full">
               <span>Blogs</span>
               <span className="dark:text-green text-crimson">
                 {blogCount || 0}
@@ -100,9 +129,9 @@ function Page() {
           <section className="mt-8">
             <h2 className="text-xl font-bold mb-4">Quick Actions</h2>
             <div className="flex gap-4">
-              <button className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 transition">
+              <Link href={'/admin/dashboard/blogs/create-blog'} className="px-4 py-2 dark:bg-white bg-slate-100 dark:text-black shadow-sm shadow-slate-600  rounded-md hover:bg-green-600 hover:dark:bg-slate-200 hover:bg-slate-200 transition">
                 Create New Blog
-              </button>
+              </Link>
               <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition">
                 Manage Users
               </button>
